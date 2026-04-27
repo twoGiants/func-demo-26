fe5a16c7a6c25c51a79d237b438bceb952e9cd50
Stanislav Jakuschevskij <stas@two-giants.com>
2026-04-27 17:15:21 +0200

    Update function files

--- handle.go
+++ handle.go
@@ -10,6 +10,7 @@ import (
 func Handle(w http.ResponseWriter, r *http.Request) {
 	/*
 	 * YOUR CODE HERE: Hello World!!!!!!! Really? Does it work? Yes?
+	 * So?
 	 *
 	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
 	 */
